bd0f395bdcf6f3645e437f2f619ef344912785a4
Sonishma <sonishmabasnet@gmail.com>
2023-02-02 00:00:29 +0545

    commit

---
+++ src/model/Main_category.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Main_category {
+
+    String sitting_arrangement;
+
+    public Main_category(String sitting_arrangement) {
+        this.sitting_arrangement = sitting_arrangement;
+    }
+
+    public String getSitting_arrangement() {
+        return sitting_arrangement;
+    }
+
+    public void setSitting_arrangement(String sitting_arrangement) {
+        this.sitting_arrangement = sitting_arrangement;
+    }
+}
